92f27f2ccf0d780f169f70b5d03a51d6bf1e9d31
Haiyue Liu <112049812+Heyweol@users.noreply.github.com>
2024-10-08 10:43:13 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  assetPrefix: isProd ? '/' : '',
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
